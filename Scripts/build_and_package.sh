5c9cb14379fba3018d5c70caaf837279420a845c
沈明昊 <shenminghao@MacBook-Pro-2.local>
2026-08-20 12:12:44 +0800

    chore(beta): set build 12 identity

--- Scripts/build_and_package.sh
+++ Scripts/build_and_package.sh
@@ -68,7 +68,7 @@ if [[ "$CONFIGURATION" == "Beta" ]]; then
     EXPECTED_BUNDLE_ID="com.shenminghao.MeetingNotes.beta"
     EXPECTED_DISPLAY_NAME="会议记录 Beta"
     EXPECTED_VERSION="1.2.0"
-    EXPECTED_BUILD="5"
+    EXPECTED_BUILD="12"
 else
     EXPECTED_BUNDLE_ID="com.shenminghao.MeetingNotes"
     EXPECTED_DISPLAY_NAME="会议记录"
